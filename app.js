646444c107cabbe48d2d1ce90e86608f930938de
Asmitasnaik <asmitanaik50@gmail.com>
2025-02-08 14:28:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//
+// function display()
+//{
+  //  return display
+//}
+//
+//Module.exports=display 
+//function display(a,b)
+
+
+
+function add(a,b)
+{
+    return a+b
+}
+function sub(a,b)
+{
+    return a-b
+}
+module.exports={add,sub}
